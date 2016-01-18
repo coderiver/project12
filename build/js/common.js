eda040a8c79a70ebaa70e217cc52770ec5f81f99
valeraderevshukov <kaktuz34@gmail.com>
2016-01-18 18:07:59 +0200

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
-	// go js
+	// open menu
+	$('.js-btn-menu').on('click', function(event){
+		var this_ 	= $(this),
+			parent 	= this_.parents('.js-menu');
+		if (parent.hasClass('is-active')) {
+			parent.removeClass('is-active');
+		}
+		else {
+			parent.addClass('is-active');
+		}
+		event.stopPropagation();
+	});
+	$('body').on('click', function(){
+		$('.js-menu').removeClass('is-active');
+	});
 
 });
