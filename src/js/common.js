1720d6a759d5f7757507d5665a2d360b9bd051e9
valeraderevshukov <kaktuz34@gmail.com>
2016-01-19 18:08:48 +0200

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
 
-	// open menu
-	$('.js-btn-menu').on('click', function(event){
-		var this_ 	= $(this),
-			parent 	= this_.parents('.js-menu');
-		if (parent.hasClass('is-active')) {
-			parent.removeClass('is-active');
-		}
-		else {
-			parent.addClass('is-active');
-		}
-		event.stopPropagation();
-	});
+	// body on click
 	$('body').on('click', function(){
 		$('.js-menu').removeClass('is-active');
+		$('.js-login').removeClass('is-active');
 	});
+
+	// open menu
+	$('.js-btn-menu').on('click', function(event){
+
+		$(this).parents('.js-menu').toggleClass('is-active');
+		$('.js-login').removeClass('is-active');
+
+		event.stopPropagation();
+	});
+
+	// open login
+	$('.js-btn-login').on('click', function(){
+		$(this).parents('.js-login').toggleClass('is-active');
+		$('.js-menu').removeClass('is-active');
+		return false;
+	});
+
+	$('.js-login').on('click', function(event){
+		event.stopPropagation();
+	});
+
+	
 
 });
