85fcf0aa5485356c7264e6f6964a26690d4c5b30
valeraderevshukov <kaktuz34@gmail.com>
2016-01-25 18:41:36 +0200

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -24,6 +24,8 @@ $(document).ready(function() {
 		$(this).parents('.js-login')
 		.find('.js-popup')
 		.toggleClass('is-active');
+
+		$(this).parents('.js-header').addClass('is-init');
 		$('.js-menu').removeClass('is-active');
 		return false;
 	});
@@ -34,10 +36,16 @@ $(document).ready(function() {
 	// popup
 	$('.js-popup-close').on('click', function(){
 		$('.js-popup').removeClass('is-active');
+		setTimeout(function() {
+			$('.js-header').removeClass('is-init');
+		}, 400);
 	});
 
 	$('.js-popup').on('click', function(){
 		$(this).removeClass('is-active');
+		setTimeout(function() {
+			$('.js-header').removeClass('is-init');
+		}, 400);
 	});
 	$('.js-popup div').on('click', function(event){
 		event.stopPropagation();
@@ -57,5 +65,38 @@ $(document).ready(function() {
 			nextArrow: '<button class="slider__btn is-next" type="button"><i class="icon icon-slider-next"></i></button>'
 		});
 	});
+
+	// paralax
+	// $('body').on('mousewheel DOMMouseScroll', function(e) {
+	//     var scrollTo = 0,
+	//     	height = $(window).height();
+	//     e.preventDefault();
+	//     if (e.type == 'mousewheel') {
+	//         scrollTo = (e.originalEvent.wheelDelta * -1);
+	//         console.log("w"+e.originalEvent.wheelDelta);
+	//         $('.js-paralax').css('margin-top', e.originalEvent.wheelDelta)
+	//     }
+	//     else if (e.type == 'DOMMouseScroll') {
+	//         scrollTo = 40 * e.originalEvent.detail;
+	//         console.log("d"+e.originalEvent.detail);
+	//         $('.js-paralax').css('margin-bottom', height / e.originalEvent.detail)
+	//     }
+	//     $(this).scrollTop(scrollTo + $(this).scrollTop());
+	    
+	// });
+	
+	$(window).on('scroll',function(e){
+	    parallaxScroll();
+	});
+	 
+	function parallaxScroll(){
+		var scrolled 	= $(window).scrollTop()
+			pos 		= 0 - (scrolled * .185) + 'px';
+		$('.js-paralax')
+		// .animate({
+		// 	'margin-top': pos
+		// }, 200)
+		.css('top', pos);
+	}
 
 });
