f82b739aa63ece4266b510614f96f5804b8dece0
valeraderevshukov <kaktuz34@gmail.com>
2016-01-28 18:00:12 +0200

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -156,4 +156,60 @@ $(document).ready(function() {
 		heightTop();
 	});
 
+	// tab
+	function tab() {
+		$(".js-tab").each(function(){
+			var tab_item 	= $(this).find(".js-tab-item"),
+				tab_link 	= tab_item .find("a"),
+				index 		= tab_link.attr("href"),
+				parents 	= $(this).parents(".js-tab-group"),
+				tab_cont 	= parents.find(".js-tab-cont");
+
+			tab_link.on("click", function() {
+				var index = $(this).attr("href");
+				$('.js-tab-item').removeClass("is-active");
+				$(this).parent().addClass("is-active");
+				tab_cont.fadeOut(0);
+				parents.find("."+index).fadeIn(300);
+
+				return false;
+			});
+
+			if (tab_item.hasClass('is-active')) {
+				var index = $('.js-tab-item.is-active a').attr("href");
+				parents.find("."+index).fadeIn(300);
+			}
+			else {
+				$(this).find('.js-tab-item:first').addClass("is-active");
+				parents.find("."+index).fadeIn(300);
+			}
+			
+		});
+	}
+	tab();
+
+	// accord
+	var acord 		= $('.js-accord'),
+		acordBlock  = $('.js-accord-block');
+	$('.js-accord-top').on('click', function(){
+		var this_ 	= $(this),
+			parent 	= this_.parents('.js-accord'),
+			block 	= parent.find('.js-accord-block');
+		if (parent.hasClass('is-active')) {
+			parent.removeClass('is-active');
+			block.slideUp(300);
+		}
+		else {
+			acord.removeClass('is-active');
+			acordBlock.slideUp(300);
+			parent.addClass('is-active');
+			block.slideDown(300);
+		}
+	});
+
+	// add class to visiting
+	$('.js-visiting').on('click', function(){
+		$(this).toggleClass('is-checked');
+	});
+
 });
