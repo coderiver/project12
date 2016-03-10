5b97f8b8dc3f38d301397fe0e0fbc668fc73de99
valeraderevshukov <kaktuz34@gmail.com>
2016-03-10 16:15:24 +0200

    upd

--- build/js/app.js
+++ build/js/app.js
@@ -2176,7 +2176,7 @@ $(document).ready(function() {
 		showMaskOnHover: false
 	});
 });
-$(window).load(function() {
+$(document).ready(function() {
 
 	var row1 = $('.js-text1');
 	var row2 = $('.js-text2');
@@ -2215,7 +2215,8 @@ $(window).load(function() {
 		dur4 = 0.5,
 		delay = 0.1,
 		delay2 = 0.05,
-		pause = 1;
+		pause = 1,
+		pause2 = 2;
 
 	function ruWordAnim(chars, dur) {
 		return TweenMax.staggerFromTo(chars, dur, {
@@ -2238,64 +2239,52 @@ $(window).load(function() {
 	}
 
 	function ruWordAnimBack(chars, dur) {
-		return TweenMax.staggerFromTo(chars, dur, {
-			rotationX: -90,
-			opacity: 0
-		}, {
+		return TweenMax.staggerTo(chars, dur, {
 			rotationX: 0,
 			opacity: 1
 		}, delay);
 	}
 
 	function enWordAnimBack(chars, dur) {
-		return TweenMax.staggerFromTo(chars, dur, {
-			rotationX: 0,
-			opacity: 1
-		}, {
+		return TweenMax.staggerTo(chars, dur, {
 			rotationX: 90,
 			opacity: 0
 		}, delay2);
 	}
 
 	function animText() {
-		setTimeout(function() {
-			var tl = new TimelineMax({ repeat: 0 })
-				.addLabel('row1:word1')
-				.add(ruWordAnim(row1.find('.ru').find('.word:first').find('.char'), dur), 'row1:word1')
-				.add(enWordAnim(row1.find('.en').find('.word:first').find('.char'), dur), 'row1:word1+=' + delay)
-				.addLabel('row1:word2', '+=' + pause)
-				.add(ruWordAnim(row1.find('.ru').find('.word:last').find('.char'), dur2), 'row1:word2')
-				.add(enWordAnim(row1.find('.en').find('.word:last').find('.char'), dur2), 'row1:word2+=' + delay)
-				.addLabel('row2:word1')
-				.add(ruWordAnim(row2.find('.ru').find('.word:first').find('.char'), dur), 'row2:word1')
-				.add(enWordAnim(row2.find('.en').find('.word:first').find('.char'), dur), 'row2:word1+=' + delay)
-				.addLabel('row2:word2')
-				.add(ruWordAnim(row2.find('.ru').find('.word:last').find('.char'), dur2), 'row2:word2')
-				.add(enWordAnim(row2.find('.en').find('.word:last').find('.char'), dur2), 'row2:word2+=' + delay)
-		}, 2000);
-		setTimeout(function() {
-			var tl = new TimelineMax({ repeat: 0 })
-				.addLabel('row1:word1')
-				.add(ruWordAnimBack(row1.find('.ru').find('.word:first').find('.char'), dur4), 'row1:word1')
-				.add(enWordAnimBack(row1.find('.en').find('.word:first').find('.char'), dur), 'row1:word1+=' + delay)
-				.addLabel('row1:word2', '+=' + pause)
-				.add(ruWordAnimBack(row1.find('.ru').find('.word:last').find('.char'), dur3), 'row1:word2')
-				.add(enWordAnimBack(row1.find('.en').find('.word:last').find('.char'), dur2), 'row1:word2+=' + delay)
-				.addLabel('row2:word1')
-				.add(ruWordAnimBack(row2.find('.ru').find('.word:first').find('.char'), dur), 'row2:word1')
-				.add(enWordAnimBack(row2.find('.en').find('.word:first').find('.char'), dur), 'row2:word1+=' + delay)
-				.addLabel('row2:word2')
-				.add(ruWordAnimBack(row2.find('.ru').find('.word:last').find('.char'), dur3), 'row2:word2')
-				.add(enWordAnimBack(row2.find('.en').find('.word:last').find('.char'), dur3), 'row2:word2+=' + delay)
-		}, 10000);
+		var tl = new TimelineMax({ repeat: 1000, paused: true })
+
+			.addLabel('row1:word1')
+			.add(ruWordAnim(row1.find('.ru').find('.word:first').find('.char'), dur), 'row1:word1')
+			.add(enWordAnim(row1.find('.en').find('.word:first').find('.char'), dur), 'row1:word1+=' + delay)
+			.addLabel('row1:word2', '+=' + pause)
+			.add(ruWordAnim(row1.find('.ru').find('.word:last').find('.char'), dur2), 'row1:word2')
+			.add(enWordAnim(row1.find('.en').find('.word:last').find('.char'), dur2), 'row1:word2+=' + delay)
+			.addLabel('row2:word1')
+			.add(ruWordAnim(row2.find('.ru').find('.word:first').find('.char'), dur), 'row2:word1')
+			.add(enWordAnim(row2.find('.en').find('.word:first').find('.char'), dur), 'row2:word1+=' + delay)
+			.addLabel('row2:word2')
+			.add(ruWordAnim(row2.find('.ru').find('.word:last').find('.char'), dur2), 'row2:word2')
+			.add(enWordAnim(row2.find('.en').find('.word:last').find('.char'), dur2), 'row2:word2+=' + delay)
+
+			.addLabel('row1:word3', '+=' + pause2)
+			.add(ruWordAnimBack(row1.find('.ru').find('.word:first').find('.char'), dur4), 'row1:word3')
+			.add(enWordAnimBack(row1.find('.en').find('.word:first').find('.char'), dur), 'row1:word3+=' + delay)
+			.addLabel('row1:word4', '+=' + pause)
+			.add(ruWordAnimBack(row1.find('.ru').find('.word:last').find('.char'), dur3), 'row1:word4')
+			.add(enWordAnimBack(row1.find('.en').find('.word:last').find('.char'), dur2), 'row1:word4+=' + delay)
+			.addLabel('row2:word3')
+			.add(ruWordAnimBack(row2.find('.ru').find('.word:first').find('.char'), dur), 'row2:word3')
+			.add(enWordAnimBack(row2.find('.en').find('.word:first').find('.char'), dur), 'row2:word3+=' + delay)
+			.addLabel('row2:word4')
+			.add(ruWordAnimBack(row2.find('.ru').find('.word:last').find('.char'), dur3), 'row2:word4')
+			.add(enWordAnimBack(row2.find('.en').find('.word:last').find('.char'), dur3), 'row2:word4+=' + delay)
+			
+		setTimeout(function() {tl.play()}, 2000);
 	} 
-	setTimeout(function() {
-		animText();
-	}, 10);
+	animText();
 	
-
-	setInterval(animText, 16000)
-
 });
 $(document).ready(function() {
 
